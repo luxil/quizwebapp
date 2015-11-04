88390f64dd66a1698fd54b6ba4dddbe66b66dd7a
lxlph <xuanlinh.do@haw-hamburg.de>
2015-11-04 10:21:17 +0100

    addQuesion&listQuestion works; editRemoveQuestion doesn't

--- admin.js
+++ admin.js
@@ -7,12 +7,13 @@ var MongoClient = require('mongodb').MongoClient;
 var dbHost = 'mongodb://localhost:27017/test';
 var myCollection = "Fragen";
 var dbConn;
+var listed = false; ///////////test
 
 var actionMenu = function(db) {
     console.log("\nWhat do you want to do?");
     console.log("[1] Add question");
-    console.log("[2] Show/Edit questions");
-    console.log("[3] Remove question");
+    console.log("[2] Show questions");
+    console.log("[3] Delete or Edit question");
     console.log("[Q] Quit programm");
     rl.question("Enter a number or a letter: ", function(input){
         switch(input.toLowerCase()){
@@ -20,10 +21,10 @@ var actionMenu = function(db) {
                 addAnswer(dbConn);
                 break;
             case "2":
-                listQuestions(dbConn);
+                listQuestions(dbConn, false);
                 break;
             case "3":
-                removeQuestion(dbConn);
+                editDeleteQuestions();
                 break;
             case "q":
                 process.exit();
@@ -76,43 +77,78 @@ function addAnswer(db){
     });
 }
 
-var listQuestions = function(db){
-    //db.collection(myCollection).find({},{},{}).toArray(
-
+var listQuestions = function(db, editTrue){
     db.collection(myCollection).find({},{},{}).toArray(
     function(err, docs){
         rl.question("Do you want to show the questions with the answers? [y] or [n]: ", function(answer){
             console.log("\nHere is the list: ");
             if (answer.toLocaleLowerCase() == "y"){
                 for(index in docs){
-                    console.log(index + ".")
+                    console.log(index + ".");
                     console.log(docs[index]);
                     console.log("\n");
                 }
             }
             else if (answer.toLowerCase() == "n"){
-                for(index in docs){
-                    console.log(index + ". " + docs[index].question);
-                }
+                for(index in docs){console.log(index + ". " + docs[index].question);}
             }
-            else {
-                console.log("Invalid input. Back to Menu");
+            else {console.log("Invalid input. Back to Menu");}
+            if(!editTrue) {
+                actionMenu(dbConn);
             }
-        actionMenu(dbConn);
         });
     }
     );
+};
+
+var editDeleteQuestions = function(db){
+    console.log("Do you want to edit the question? [y] or [n]: ");
+    rl.question('',function(answer){
+        switch(answer.toLowerCase()){
+            case 'y':
+                rl.question("Do you want to change[c] or delete[d] a question? ", function(answer2){
+                    switch(answer2.toLowerCase()){
+                        case 'c':
+                            changeQuestion();
+                            break;
+                        case 'd':
+                            listQuestions(dbConn, true);
+                            deleteQuestion();
+                            break;
+                        case 'q':
+                            actionMenu(dbConn);
+                            break;
+                        default:
+                            console.log("Invalid input. Back to Menu.");
+                            actionMenu(dbConn);
+                            break;
+                    }
+                })
+                break;
+        }
+    });
 }
 
-var removeQuestions = function(db){
-
+var deleteQuestion = function(db){
+    console.log("Which question do you want to delete? Enter the number: ");
+    listQuestions(dbConn, true);
+    rl.question('', function (answer) {
+        for (index in questions) {
+            if (index === answer) {
+                db.collection(myCollection).deleteOne(questions[index]);
+                console.log("Succesfully removed");
+            } else {
+                console.log("Error: There is no question for this number.");
+                actionMenu();
+            }
+        }
+    });
 }
 
 MongoClient.connect(dbHost, function(err, db){
     if ( err ) throw err;
     dbConn = db;
     actionMenu();
-    console.log("connect");
 });
 
 

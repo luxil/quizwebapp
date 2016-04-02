873329a44ea850d8f80258df8963f1af3a87a480
Byron <bisonp86@gmail.com>
2016-04-02 17:20:42 +0200

    Change from dbsimulator to proper db connection. Not fully working yet.

--- monitor.js
+++ monitor.js
@@ -8,6 +8,8 @@ function monitor(){
     var C = document.getElementById("C");
     var D = document.getElementById("D");
 
+    var test;
+
     socket.on('timerUpdate',function(data){
 
        timer.innerHTML = "<h3>"+ data + "</h3>";
@@ -18,6 +20,88 @@ function monitor(){
         B.innerHTML = data[2];
         C.innerHTML = data[3];
         D.innerHTML = data[4];
+
+        test = data[5];
     });
 
+    socket.on('updateScore',function(){
+        window.location.replace("/success");
+    });
+
+    socket.on('answerShow',function(data){
+        checkAnswer(data);
+        console.log("answer SHow aufgerufen");
+    });
+
+    var toggled = true;
+    function aFunction(){
+        console.log("aFunction");
+        if(toggled){
+            A.backgroundColor = "red";
+            toggled = false;
+        }else{
+            A.backgroundColor = "#DFF0D8";
+            toggled = true;
+        }
+    }
+    function bFunction(){
+        console.log("bFunction");
+        if(toggled){
+            A.backgroundColor = "red";
+            toggled = false;
+        }else{
+            A.backgroundColor = "#D9EDF7";
+            toggled = true;
+        }
+    }
+    function cFunction(){
+        console.log("cFunction");
+        if(toggled){
+            A.backgroundColor = "red";
+            toggled = false;
+        }else{
+            A.backgroundColor = "#FCF8E3";
+            toggled = true;
+        }
+    }
+    function dFunction(){
+        console.log("dFunction");
+        if(toggled){
+            A.backgroundColor = "red";
+            toggled = false;
+        }else{
+            A.backgroundColor = "#F2DEDE";
+            toggled = true;
+        }
+    }
+
+    var checkAnswer = function(answer){
+        if (test == answer){
+            console.log("antwort korrekt uebermittelt");
+        }
+        if(A.innerHTML == answer){
+
+            var aInterval = setInterval(aFunction,500);
+            //setTimeout(clearInterval(aInterval),3000);
+
+        }else if (B.innerHTML == answer){
+
+
+
+           // A.innerHTML = " ";
+           // C.innerHTML = " ";
+           // D.innerHTML = " ";
+        }else if (C.innerHTML == answer){
+
+            //A.innerHTML = " ";
+            //B.innerHTML = " ";
+            //D.innerHTML = " ";
+        }else if (D.innerHTML == answer) {
+
+            //A.innerHTML = " ";
+            //B.innerHTML = " ";
+            //C.innerHTML = " ";
+
+        }
+    }
 }
--- quizServer.js
+++ quizServer.js
@@ -11,6 +11,7 @@ exports.init = function(fragen,nio){
     return aktuelleFrage;
   };
   var timer = 15;
+  var timer2 = 4;
   // Fragen vorbereiten
 
   var Fragen = [];
@@ -27,7 +28,7 @@ exports.init = function(fragen,nio){
     //Letztes Element ist die richtige Antwort
     frage.push(frage[1]);
 
-    //Shuffle der Antwortmöglichkeiten
+    //Shuffle der Antwortmï¿½glichkeiten
     random.push(frage[1],frage[2],frage[3],frage[4]);
     shuffle(random);
     for (var k = 0;k<4;k++){
@@ -36,7 +37,7 @@ exports.init = function(fragen,nio){
     Fragen.push(frage);
   }
 
-  // Timer Funktionalität
+  // Timer Funktionalitï¿½t
   function showTimer(){
     io.sockets.emit('timerUpdate',counter);
   };
@@ -49,42 +50,61 @@ exports.init = function(fragen,nio){
   function getAnswer(){
     io.sockets.emit('answerUpdate');
   }
+  function showAnswer(){
+    io.sockets.emit('answerShow',aktuelleFrage[5]);
+  }
 
   console.log(Fragen);
 
 
   // Logik starten
-
+  var pause = false;
 
   var counter = timer;
+  var counter2 = timer2;
   function logic(){
 
-    if (counter == timer){
+    if (counter == timer && pause == false){
       console.log("Hier wird die Frage gepickt.");
       anzahl -= 1;
-      // Momentan nur anzeigen und löschen der Frage
+      // Momentan nur anzeigen und lï¿½schen der Frage
       aktuelleFrage = Fragen.pop();
       showQuestion();
       console.log(aktuelleFrage);
     }
-    if (counter <= 0){
+    if (counter <= 0 && pause == false){
       console.log(counter);
       showTimer();
       console.log("Jetzt werden Antworten ausgewertet und Punkte vergeben.");
       getAnswer();
-      // Hier kommt ein Even um die aktuelle Antwort auszuwerten
+      // Hier kommt ein Event um die aktuelle Antwort auszuwerten
+      pause = true;
+      console.log("pause wurde true.");
       counter = timer;
       if (anzahl == 0){
         clearInterval(interval);
         console.log("Quiz beendet");
 
-        //showScore();
-        // Hier kommt ein Event um display auf scoreboard zu switchen
+        showScore();
+
       }
-    }else{
+    }else if(pause == false){
       console.log(counter);
       showTimer();
       counter -= 1;
+    }
+    else if (pause == true){
+      if(counter2 <= 0){
+        pause = false;
+        console.log("pause wurde gefalsed");
+        counter2 = timer2;
+      }else if(counter2 == timer2){
+        showAnswer();
+        counter2 -= 1;
+      }else{
+        counter2 -=1;
+      }
+
     }
   };
   var interval = setInterval(logic,1000);
